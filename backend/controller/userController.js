53cce2449a39a4d94fbc27e8f2731080e265ef90
Nitin <118925609+Nitin-NK-01@users.noreply.github.com>
2024-04-05 13:23:27 +0530

    user login and authtoken generated

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,25 +1,43 @@
-import {User,validate} from "../models/user.js";
+import { User, validate } from "../models/user.js";
 import bcrypt from "bcrypt";
 
 
-export const createUser=async(req,res)=>{
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send({ message: error.message })
+export const createUser = async (req, res) => {
+    try {
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send({ message: error.message })
 
-    const {name, email, password}=req.body;
-    const user=await User.findOne({email})
-    if (user) return res.status(403).send({ message: "user already exits" });
+        const user = await User.findOne({ email: req.body.email })
+        if (user) return res.status(403).send({ message: "user already exits" });
 
-    const salt = await bcrypt.genSalt(Number(process.env.SALT));
-    const hashPass = await bcrypt.hash(req.body.password, salt)
-    let newUser = await new User({
-        ...req.body,
-        password: hashPass,
-    }).save();
-    newUser.password = undefined;
-    newUser.__v = undefined;
+        const salt = await bcrypt.genSalt(Number(process.env.SALT));
+        const hashPass = await bcrypt.hash(req.body.password, salt)
+        let newUser = await new User({
+            ...req.body,
+            password: hashPass,
+        }).save();
+        newUser.password = undefined;
+        newUser.__v = undefined;
 
-    // const token = newUser.generateAuthToken();
+        const token = newUser.generateAuthToken();
 
-    res.status(200).send({ data: newUser, message: "user is created" })
+        res.status(200).send({token, data: newUser, message: "user is created" })
+    } catch (e) {
+        res.status(500).send('Internal server error')
+    }
+}
+
+export const loginUser = async (req, res) => {
+    try{
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) return res.status(401).send({ message: "Email or Password is incorrect!" });
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) return res.status(401).send({ auth: false, token: null, message: "Email or Password is incorrect!" });
+
+        const token = user.generateAuthToken();
+
+        res.send({ date: token, massaege: 'signing in please wait' });
+    } catch (e) {
+        res.status(500).send('Internal server error')
+    }
 }
--- backend/routes/authuser.js
+++ backend/routes/authuser.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { createUser } from "../controller/userController.js";
+import { createUser,loginUser } from "../controller/userController.js";
 
 const router = express.Router()
 
 // 1. create user
 router.post('/createuser', createUser)
-
+// 2. login user
+router.post('/login', loginUser)
+// 3.  get all users (admin only)
+// router.get("/", isAuthenticated ,(req,res)=>{
+//     User.find({},"-__v")
+//         .then((users)=> res.json(users))
+// })
 export default router;
