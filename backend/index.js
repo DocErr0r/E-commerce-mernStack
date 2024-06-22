45496ac26bf034a3e9f6228209ccf042576b9bab
Nitin <118925609+Nitin-NK-01@users.noreply.github.com>
2024-06-22 16:30:39 +0530

    for development use proxy rether then cors

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import connectTODB from './config/db.js';
 import userRouter from './routes/authuser.js';
 import productRouter from './routes/productRoutes.js';
 import cookieParser from 'cookie-parser';
-import cors from "cors";
+// import cors from "cors";
 
 dotenv.config({ path: '../.env' });
 // dotenv.config();
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+// app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server:{
-  //   proxy:{
-  //     '/api/':{
-  //       target:'http//localhost:6000',
-  //       changeOrigin:true,
-  //       rewrite:(path)=>path.replace(/^\/api/,'')
-  //     }
-  //   }
-  // }
+  server:{
+    proxy:{
+      '/api/':'http://localhost:5678'
+      // '/api/':{
+      //   target:'http://localhost:5678',
+      //   changeOrigin:true,
+      //   rewrite:(path)=>path.replace(/^\/api/,'')
+      // }
+    }
+  }
 })
