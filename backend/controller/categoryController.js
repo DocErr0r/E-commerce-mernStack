6586fca55cdc71a2775d074986293b836fa64aeb
Nitin <118925609+Nitin-NK-01@users.noreply.github.com>
2024-07-05 20:15:12 +0530

    adding category CRUD option routes

---
+++ backend/controller/categoryController.js
@@ -0,0 +1,75 @@
+import serverHandler from "../middlewares/serverhandler.js";
+import Category from "../models/category.js";
+
+export const creactCategory = serverHandler(async (req, res) => {
+    try {
+        const { name } = req.body
+
+        const exist = await Category.findOne({ name })
+        if (exist) {
+            return res.status(400).send({ message: "category already exist" })
+        }
+        const category = await new Category(req.body).save()
+        res.status(200).send({ date: category })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error)
+    }
+})
+
+export const updateCategory = serverHandler(async (req, res) => {
+    try {
+        const { name } = req.body
+        const category = await Category.findById(req.params.id)
+        if (!category) {
+            return res.status(400).send({ message: "category not found" })
+        }
+
+        // category.name=name
+        const newCategory = await Category.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+
+        res.status(200).send({ date: newCategory })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error)
+    }
+})
+
+export const removeCategory = serverHandler(async (req, res) => {
+    try {
+        const { name } = req.body
+        const category = await Category.findById(req.params.id)
+        if (!category) {
+            return res.status(400).send({ message: "category not found" })
+        }
+
+        const remove = await Category.findByIdAndDelete(req.params.id)
+
+        res.status(200).send({ data: remove, message: "Category Deleted" })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error)
+    }
+})
+
+export const getCategories = serverHandler(async (req, res) => {
+    try {
+        const categories = await Category.find()
+        if (!categories) return res.status(400).send({ message: 'no such category found' });
+        res.status(200).send({ data: categories })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error)
+    }
+})
+
+export const getCategory = serverHandler(async (req, res) => {
+    try {
+        const category = await Category.findById(req.params.id)
+        if (!category) return res.status(400).send({ message: 'no such category found' });
+        res.status(200).send({ data: category })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message)
+    }
+})
