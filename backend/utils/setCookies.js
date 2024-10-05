0e20af636e2f9da2e1d4d29a5acb16af967bce49
Nitin <118925609+Nitin-NK-01@users.noreply.github.com>
2024-10-05 21:08:35 +0530

    backend deployed

--- backend/utils/setCookies.js
+++ backend/utils/setCookies.js
@@ -1,7 +1,7 @@
 const setCookies = (res, token) => {
     // set token in cookie
     res.cookie("authToken", token,
-        { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'strict', maxAge: 60 * 60 * 1000 }
+        { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', maxAge: 60 * 60 * 1000 }
     )
     // .setHeader('Authorization', `Bearer ${token}`)
 }
